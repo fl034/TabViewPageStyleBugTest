01247f3c2db4a0c90b4be4e0c76f8088be76d700
Frank Lehmann <frank.lehmann@bdr.de>
2023-04-03 16:32:39 +0200

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+enum Tab: String, Identifiable, Hashable, CaseIterable {
+    case green, yellow, red
+    
+    var id: String { rawValue }
+}
+
+struct ContentView: View {
+    // Starting with last element. Changing this to .green doesn't work,
+    // Except when swiped to or yellow was called once.
+    @State var selection: Tab = .red
+    
+    var body: some View {
+        NavigationStack {
+            TabView(selection: $selection) {
+                Color.green.opacity(0.5)
+                    .tag(Tab.green)
+                Color.yellow.opacity(0.5)
+                    .tag(Tab.yellow)
+                Color.red.opacity(0.5)
+                    .tag(Tab.red)
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .navigationTitle("Test")
+            .toolbar {
+                ToolbarItem {
+                    Picker(selection: $selection) {
+                        ForEach(Tab.allCases) { element in
+                            Text(element.rawValue)
+                                .tag(element)
+                        }
+                    } label: {
+                        Text(selection.rawValue)
+                    }
+                    .pickerStyle(.menu)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabViewPageStyleBugTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewPageStyleBugTestApp.swift
+//  TabViewPageStyleBugTest
+//
+//  Created by adm-P00800922 on 03.04.23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewPageStyleBugTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
